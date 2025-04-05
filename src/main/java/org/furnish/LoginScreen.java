ae9f44405fb31ee06366631bcda194a3cea5a23f
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-04-05 21:22:39 +0530

    feat: Add close button util class

--- src/main/java/org/furnish/LoginScreen.java
+++ src/main/java/org/furnish/LoginScreen.java
@@ -1,6 +1,9 @@
 package org.furnish;
 
 import javax.swing.*;
+
+import org.furnish.utils.CloseButtonUtil;
+
 import java.awt.*;
 import java.awt.geom.RoundRectangle2D;
 
@@ -36,7 +39,8 @@ public class LoginScreen extends JFrame {
         add(mainPanel);
 
         // Close button
-        JButton closeButton = createCloseButton();
+        JButton closeButton = CloseButtonUtil.createCloseButton();
+
         JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         topPanel.setOpaque(false);
         topPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
@@ -200,44 +204,6 @@ public class LoginScreen extends JFrame {
         });
 
         return passwordField;
-    }
-
-    // Custom close button
-    private JButton createCloseButton() {
-        JButton closeButton = new JButton();
-        closeButton.setContentAreaFilled(false);
-        closeButton.setBorderPainted(false);
-        closeButton.setFocusPainted(false);
-        closeButton.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
-
-        try {
-            ImageIcon closeIcon = new ImageIcon(getClass().getResource("./images/close.png"));
-            if (closeIcon.getImage() != null) {
-                closeButton.setIcon(closeIcon);
-            } else {
-                closeButton.setText("✕");
-                closeButton.setFont(new Font("Arial", Font.PLAIN, 18));
-            }
-        } catch (Exception e) {
-            closeButton.setText("✕");
-            closeButton.setFont(new Font("Arial", Font.PLAIN, 18));
-        }
-
-        closeButton.setForeground(Color.WHITE);
-        closeButton.setRolloverEnabled(true);
-
-        closeButton.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                closeButton.setForeground(new Color(255, 100, 100));
-            }
-
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                closeButton.setForeground(Color.WHITE);
-            }
-        });
-
-        closeButton.addActionListener(e -> System.exit(0));
-        return closeButton;
     }
 
     private boolean authenticate(String username, String password) {
